30343576826dc8994b2bb6bbc2cd9bca9ff8b427
St Louis County <blord-castillo@stlouisco.com>
2014-04-30 14:06:56 -0500

    Update wifi.js

--- wifi.js
+++ wifi.js
@@ -7,5 +7,5 @@ wifi(){
   {"geometry": {"type": "Point", "coordinates": [38.64908, -90.3032]}, "type": "Feature", "id": "3", "properties": {"Id": 0, "siteid": "A-00002", "building": "Whitaker Hall", "wifitype": "A", "range": 100}}, 
   {"geometry": {"type": "Point", "coordinates": [38.64804, -90.30502]}, "type": "Feature", "id": "4", "properties": {"Id": 0, "siteid": "A-00001", "building": "Brookings Hall", "wifitype": "G", "range": 400}}, 
   {"geometry": {"type": "Point", "coordinates": [38.64848, -90.30551]}, "type": "Feature", "id": "5", "properties": {"Id": 0, "siteid": "A-00004", "building": "Beaumont Pavillion", "wifitype": "B", "range": 200}}
-  ]}
+  ]};
 }
